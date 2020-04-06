146ce04b5adcec7e79142753240ab0671f322bf7
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-06 18:57:38 +0100

    add react-router-dom code, delete start code for linking and tracking components, pages render if you type in the correct url but no links yet

--- react_router/react_router_mine/src/components/Main.js
+++ react_router/react_router_mine/src/components/Main.js
@@ -3,73 +3,20 @@ import About from "./About";
 import Home from "./Home";
 import Pricing from "./Pricing";
 import Contact from "./Contact";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class Main extends Component {
-  constructor() {
-    super();
-    this.state = { page: null };
-
-    this.gotoHome = this.gotoHome.bind(this);
-    this.gotoAbout = this.gotoAbout.bind(this);
-    this.gotoPricing = this.gotoPricing.bind(this);
-    this.gotoContact = this.gotoContact.bind(this);
-  }
-
   render() {
-    const child = this.pageComponent();
     return (
-      <div>
-        <h4>Main App</h4>
-        <ul>
-          <li>
-            <a onClick={this.gotoHome}>Home</a>
-          </li>
-          <li>
-            <a onClick={this.gotoAbout}>About</a>
-          </li>
-          <li>
-            <a onClick={this.gotoPricing}>Pricing</a>
-          </li>
-          <li>
-            <a onClick={this.gotoContact}>Contact</a>
-          </li>
-        </ul>
-        {child}
-      </div>
+      <Router>
+        <React.Fragment>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/pricing" component={Pricing} />
+          <Route path="/contact" component={Contact} />
+        </React.Fragment>
+      </Router>
     );
-  }
-
-  pageComponent() {
-    switch (this.state.page) {
-      case "/about":
-        return <About />;
-      case "/pricing":
-        return <Pricing />;
-      case "/contact":
-        return <Contact />
-      default:
-        return <Home />;
-    }
-  }
-
-  gotoHome(event) {
-    event.preventDefault();
-    this.setState({ page: "/home" });
-  }
-
-  gotoAbout(event) {
-    event.preventDefault();
-    this.setState({ page: "/about" });
-  }
-
-  gotoPricing(event) {
-    event.preventDefault();
-    this.setState({ page: "/pricing" });
-  }
-
-  gotoContact(event) {
-    event.preventDefault();
-    this.setState({ page: "/contact"});
   }
 }
 
