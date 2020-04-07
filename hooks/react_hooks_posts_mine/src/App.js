22dfa61e25ed0abc77c8a03d7d543ea24c6a600f
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-07 19:35:03 +0100

    complete hooks lesson

--- hooks/react_hooks_posts_mine/src/App.js
+++ hooks/react_hooks_posts_mine/src/App.js
@@ -3,14 +3,24 @@ import React, { useState, useEffect } from 'react';
 export default function App(){
 
   const [post, setPost] = useState({})
+  const [id, setId] = useState(1)
+  const [windowSize, setWindowSize] = useState(window.innerWidth)
 
   useEffect(() => {
     getPost()
-  }, [])
+    window.addEventListener('resize', handleWindowResize)
+
+  return () => window.removeEventListener('resize', handleWindowResize);
+
+  }, [id])
+
+  const handleWindowResize = () => {
+    setWindowSize(window.innerWidth)
+  }
 
   const getPost = () => {
     console.log('fetching');
-    fetch(`http://jsonplaceholder.typicode.com/posts/1`)
+    fetch(`http://jsonplaceholder.typicode.com/posts/${id}`)
     .then(res => res.json())
     .then(res => setPost(res))
   }
@@ -19,6 +29,9 @@ export default function App(){
     <>
     <h2>{post.title}</h2>
     <p>{post.body}</p>
+    <button onClick={() => setId(id + 1)}>Next Post</button>
+
+    <p>{windowSize < 420 ? 'phone' : 'desktop'}</p>
     </>
   );
 }
